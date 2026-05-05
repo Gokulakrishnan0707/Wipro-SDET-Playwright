e4be494bc9dc47e2eec45e73053c5407a5353fba
Gokulakrishnan0707 <gkgokul0708@gmail.com>
2026-05-05 12:38:45 +0530

    fetch

--- Day4/index.js
+++ Day4/index.js
@@ -18,3 +18,16 @@ getData().then((data) => {
 // setInterval(() => {
 //     console.log("Checking for new data...");
 // }, 2000);
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json()) // res -> string -> json)
+    .then(data => data.filter(d => d.id % 2 === 0))
+    .then(data => data.map(d => (d.title = "Aaryan " + d.title)))
+    .then(data => console.log(data))
+    .catch(error => console.error("Error fetching data:", error));
+ 
+// getData().then((data) => {
+//     console.log(data);
+// }).catch((error) => {
+//     console.error("Error:", error);
+// });
